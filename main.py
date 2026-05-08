5c1a159fd3dc30bf71808d3b40dacca18d693c0d
Danny Kamohelo Ntshala <ntshaladannykamohelo@gmail.com>
2026-05-08 06:27:06 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import requests
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+News_Api_key = ''
+
+OWNM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = ""
+account_sid = ""
+auth_token = ""
+
+parameter = {
+    "function":"TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey":"0ELFQOZFWKB8JIDH"
+}
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+
+response = requests.get(url= STOCK_ENDPOINT,params=parameter)
+data = response.json()["Time Series (Daily)"]
+
+data_list = [value for (key,value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data['4. close']
+print(yesterday_closing_price)
+
+day_before_yesterday = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday['4. close']
+print(day_before_yesterday_closing_price)
+
+difference = float(yesterday_closing_price)- float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+diff_percent = round((difference/float(yesterday_closing_price))*100)
+print(diff_percent)
+
+if abs(diff_percent) >1:
+    news_params = {
+        "apikey": News_Api_key,
+        "qInTitle": COMPANY_NAME
+    }
+    news_repsonse = requests.get(NEWS_ENDPOINT,params=news_params)
+    articles = news_repsonse.json()['articles']
+
+    three_articles = articles[:3]
+    print(three_articles)
+
+    formated_articles =[f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {articles['title']}. \nBrief: {articles['description']}" for articles in three_articles]
+
+    client = Client(account_sid,auth_token)
+
+    for articles in formated_articles :
+        message = client.messages.create(
+            from_="whatsapp:",
+            body=articles,
+            to="whatsapp:"
+        )
+
+        print(message.status)
